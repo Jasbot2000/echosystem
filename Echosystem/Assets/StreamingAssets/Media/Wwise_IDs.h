54ee80767552eb4076d6a399a945811f56e00159
Jasbot2000 <jasper.arseniev.johnson@gmail.com>
2026-04-12 18:46:16 -0500

    Added Cicadas, Thunder, River, (Wind base layer)

---
+++ Echosystem/Assets/StreamingAssets/Media/Wwise_IDs.h
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BIRD = 1544377644U;
+        static const AkUniqueID BUG = 714721617U;
+        static const AkUniqueID CICADASBACKGROUND = 2093786283U;
+        static const AkUniqueID CICADASFOREGROUND = 31267972U;
+        static const AkUniqueID FROG = 2492899947U;
+        static const AkUniqueID PLAYRANDOM = 2409657758U;
+        static const AkUniqueID RIVER = 3605787649U;
+        static const AkUniqueID THUNDER = 186852181U;
+        static const AkUniqueID WINDBASE = 4264094620U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID FREAKINGREALBANK = 2627485820U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
